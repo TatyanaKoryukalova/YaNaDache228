6379c4413d2b2185b32d53d5f0981ab144be240a
Tatyana Koryukalova <tata.koryukalova@yandex.ru>
2021-09-07 22:58:12 +0500

    Commit basic Java elements

---
+++ src/com/gmail/san666/yoshi/Dacha228Main.java
@@ -0,0 +1,22 @@
+package com.gmail.san666.yoshi;
+
+import java.util.Scanner;
+
+public class Dacha228Main {
+    public static void main(String[] args) {
+        //Hey bitches
+        System.out.println("Hey bitches");
+        int value1 = 0;
+        int value2 = 5;
+        int sum = value1 + value2;
+        int multiplication = value1 * value2;
+        int difference = value1 - value2;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Разность: " + difference);
+        //Scanner scanner = new Scanner(System.in);
+        String userInput = new Scanner(System.in).next();
+        System.out.println("Ввод данных пользователем: "+userInput);
+
+    }
+}
