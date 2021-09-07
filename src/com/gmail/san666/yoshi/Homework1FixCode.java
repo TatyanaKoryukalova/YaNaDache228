53fce444491d892831d8bdee9ceed4a89f01a99a
Tatyana Koryukalova <tata.koryukalova@yandex.ru>
2021-09-07 23:19:14 +0500

    Just commiting code of homework

---
+++ src/com/gmail/san666/yoshi/Homework1FixCode.java
@@ -0,0 +1,18 @@
+package com.gmail.san666.yoshi;
+
+public class Homework1FixCode {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 8;
+        sum(a, b);  // 14
+        sum(3, a);  // 9
+        sum(5, 23); // 28
+    }
+
+    static void sum(int x, int y) {
+
+        int z = x + y;
+        System.out.println(z);
+
+    }
+}
