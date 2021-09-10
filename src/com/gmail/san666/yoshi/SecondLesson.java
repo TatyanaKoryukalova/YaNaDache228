2b61dd4e9eb8daf6229aeeb1fc4d4b8fdba76568
Tatyana Koryukalova <tata.koryukalova@yandex.ru>
2021-09-10 22:19:11 +0500

    Conditional statements

---
+++ src/com/gmail/san666/yoshi/SecondLesson.java
@@ -0,0 +1,13 @@
+package com.gmail.san666.yoshi;
+
+import java.util.Scanner;
+
+public class SecondLesson {
+    public static void main(String[] args) {
+            int userInput = new Scanner(System.in).nextInt();
+            if(userInput%2==0){
+                System.out.println("Число четное");
+            } else System.out.println("Число нечетное");
+
+    }
+}
