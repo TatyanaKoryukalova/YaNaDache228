effeb7d60f02ef0f0fe1e274848d9becb4b11d1f
TatyanaKoryukalova <tata.koryukalova@yandex.ru>
2021-11-21 20:42:01 +0500

    Tests working

--- src/test/java/com/gmail/san666/yoshi/oop/CompanyTest.java
+++ src/test/java/com/gmail/san666/yoshi/oop/CompanyTest.java
@@ -21,6 +21,17 @@ class CompanyTest {
 
     @Test
     void firePersonTest() {
+        Person alex = new Person("Alex", 35);
+        Person bob = new Person("Bob", 27);
+        Company yahoo = new Company("Yahoo");
+        yahoo.hirePerson(alex,15000);
+        yahoo.hirePerson(bob,35000);
+        yahoo.firePerson(alex);
+
+        assertThat(yahoo.getStaff().length).isEqualTo(1);
+        assertThat(bob).isEqualTo(yahoo.getStaff()[0]);
+        assertThat(alex.getSalary()).isEqualTo(0);
+        assertThat(alex.getCompanyName()).isEqualTo("This person doesn't work anywhere");
 
     }
 
